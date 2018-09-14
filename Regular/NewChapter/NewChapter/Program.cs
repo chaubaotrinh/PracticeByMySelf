fae9b622143258b16abdba3d5685971aeff6b171
Chau Bao Trinh <chau.trinh@my.tccd.edu>
2018-09-14 14:32:04 -0500

    First 2 steps

---
+++ Regular/NewChapter/NewChapter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewChapter
+{
+    class Program
+    {
+        static void Main( string[] args )
+
+        {
+            //How to work on github
+            string step1 = "Create a folder and file on Github";
+            Console.WriteLine(step1);
+            string step2 = "Clone the repository in visual studio";
+            Console.WriteLine(step2);
+
+        }
+    }
+}
